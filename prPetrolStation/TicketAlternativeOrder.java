90d9b19730ff5179cb3d73d0acd729710ca3d376
Illya Rozumovskyy <panvalkon@gmail.com>
2021-06-24 17:24:38 +0200

    I think is okay

--- prPetrolStation/TicketAlternativeOrder.java
+++ prPetrolStation/TicketAlternativeOrder.java
@@ -5,13 +5,13 @@ import java.util.Comparator;
 public class TicketAlternativeOrder implements Comparator<Ticket> {
 
 	public TicketAlternativeOrder() {
-		
+
 	}
-	
+
 	@Override
 	public int compare(Ticket o1, Ticket o2) {
 		int result = Integer.compare(o1.getNumTicket(), o2.getNumTicket()) * -1;
-		if (result ==0) {
+		if (result == 0) {
 			result = o1.getStation().compareToIgnoreCase(o2.getStation());
 		}
 		return result;
